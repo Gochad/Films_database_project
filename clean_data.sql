c3fe83f6c27de520ee8f5d7dffaed0738a5f0003
Gochad <malaga.drag@gmail.com>
2022-12-27 13:48:21 +0100

    insert app dir

---
+++ clean_data.sql
@@ -0,0 +1,19 @@
+DROP TABLE Productions
+DROP TABLE Person_awards
+DROP TABLE Production_availability
+DROP TABLE Award_ceremonies
+DROP TABLE Cinemas
+DROP TABLE Movies
+DROP TABLE People
+DROP TABLE Production_awards
+DROP TABLE Production_statistics
+DROP TABLE Series
+DROP TABLE Soundtracks
+DROP TABLE Staff
+DROP TABLE Staff_description
+DROP TABLE Streaming_platforms
+DROP TABLE Roles
+DROP TABLE Screenings
+
+
+
